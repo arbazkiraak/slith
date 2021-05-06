46194055cbfaf6c8cecf1ef3861671c906b1a46f
root <root@kiraakboy.localdomain>
2021-05-06 08:14:58 +0530

    previous-check

--- src/index.ts
+++ src/index.ts
@@ -77,11 +77,20 @@ const prepare = async () => {
   await exec.exec("sudo add-apt-repository ppa:ethereum/ethereum -y");
   await exec.exec("sudo apt-get install solc -y");
 
-  core.info("Downloading slither");
-  await exec.exec("git clone https://github.com/trailofbits/slither");
+  await exec.exec(`wget https://github.com/crytic/slither/archive/${slitherVersion}.zip -O /tmp/slither.zip`);
 
-  await exec.exec("cd slither");
-  await exec.exec("python3 setup.py develop");
+
+  core.info("Unzipping slither");
+  await exec.exec(" unzip /tmp/slither.zip -d .");
+
+
+  core.info("Installing slither");
+  await exec.exec("sudo python3 setup.py develop");
+  // core.info("Downloading slither");
+  // await exec.exec("git clone https://github.com/trailofbits/slither");
+
+  // await exec.exec("cd slither");
+  // await exec.exec("python3 setup.py develop");
 
 }
 
